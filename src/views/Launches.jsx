38a3e5c440f4040ac7650d0e1f22e9e2a0726d2e
Dacredible <jikun.95.02@gmail.com>
2019-09-23 18:25:05 -0700

    style update for latest launch

--- src/views/Launches.jsx
+++ src/views/Launches.jsx
@@ -19,23 +19,44 @@ export default class Launches extends Component {
 
   displayTag = (isSuccess) => {
     if (isSuccess) {
-      return <div className="launch-status status-success">Successful</div>;
+      return <div className="launch__status launch__status--success">Successful</div>;
     }
-    return <div className="launch-status status-fail">Fail</div>;
+    return <div className="launch__status launch__status--fail">Fail</div>;
   };
 
   render() {
     const { query, name } = this.state;
     const launchesArray = LaunchesData(query, name);
     const latestLaunch = launchesArray[0];
+    console.log(latestLaunch);
     return (
       <main>
-        <section className="launch-latest">
+        <section className="launch--latest">
+          <div className="launch--latest-title">Latest Launch</div>
           <div className="row">
-            <h3>#{latestLaunch.flight_number}</h3>
-            <img src={latestLaunch.links.mission_patch_small} alt="" />
+            <h3 className="launch__filght-number">#{latestLaunch.flight_number}</h3>
+            <img className="launch__patch" src={latestLaunch.links.mission_patch_small} alt="" />
             {this.displayTag(latestLaunch.launch_success)}
           </div>
+          <div className="launch__title">{latestLaunch.mission_name}</div>
+          <div className="row">
+            <div className="launch__detail">
+              <h4>Rocket</h4>
+              <p>{latestLaunch.rocket.rocket_name}</p>
+            </div>
+            <div className="launch__detail">
+              <h4>Date</h4>
+              <p>{latestLaunch.launch_date_local}</p>
+            </div>
+            <div className="launch__detail">
+              <h4>Site</h4>
+              <p>{latestLaunch.launch_site.site_name}</p>
+            </div>
+          </div>
+          <div className="row">
+            <p>{latestLaunch.details}</p>
+          </div>
+          <div className="past-launches">Past Launches</div>
         </section>
         <section className="launch-list">list</section>
       </main>
